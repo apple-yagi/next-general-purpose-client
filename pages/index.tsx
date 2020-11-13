f0641f91f584cc769a1055502699a7b95f6f6104
apple-yagi <applesryu@gmail.com>
2020-11-13 22:55:31 +0900

    TypeScript化

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react"
+
+const Index: FC = () => {
+  return (
+    <div className="mx-auto md:flex">
+      <img src="https://cdn.svgporn.com/logos/nextjs.svg" alt="nextjs icon" />
+      <img src="https://cdn.svgporn.com/logos/tailwindcss-icon.svg" alt="tailwind icon" />
+    </div>
+  )
+}
+
+export default Index
