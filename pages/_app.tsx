2bfd2f666d22c5b4474cfe8df60fca82ccf642bb
apple-yagi <applesryu@gmail.com>
2020-11-14 17:22:46 +0900

    add material-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { AppProps } from 'next/app';
+import PropTypes from 'prop-types';
 import '../styles/globals.css'
 import '../styles/tailwind.css'
+import Header from '../components/navigators/Header';
 
-const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+const App = (props: AppProps) => {
+  const { Component, pageProps } = props;
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <Fragment>
+      <Header />
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </Fragment>
+  )
 }
+
+App.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
 
 export default App
